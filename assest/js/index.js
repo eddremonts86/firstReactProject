8dbee04090bf4217d5d61c8c17e3a700d0fcdfe9
Edd Remonts <eddremonts86@gmail.com>
2020-06-29 13:24:33 +0200

    nav bar

---
+++ assest/js/index.js
@@ -0,0 +1,15 @@
+const domContainer = document.querySelector("#app");
+ReactDOM.render(
+  <nav class="navBar">
+    <span class="logo">logo</span>
+    <ul class="menu">
+      <li>Home</li>
+      <li>About me</li>
+      <li>Services</li>
+      <li>Contact us</li>
+    </ul>
+  </nav>,
+  domContainer
+);
+
+ 
