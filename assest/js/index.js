07a5d2c90a4b26e32eddd09c0954c97897425f23
Edd Remonts <eddremonts86@gmail.com>
2020-07-01 08:55:20 +0200

    new nabvar

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,15 +1,3 @@
 const domContainer = document.querySelector("#app");
-ReactDOM.render(
-  <nav class="navBar">
-    <span class="logo">logo</span>
-    <ul class="menu">
-      <li>Home</li>
-      <li>About me</li>
-      <li>Services</li>
-      <li>Contact us</li>
-    </ul>
-  </nav>,
-  domContainer
-);
-
- 
+import NavBar from "components/NavBar";
+ReactDOM.render(<NavBar/>, domContainer);
---
+++ components/NavBar.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function NavBar() {
+  return (
+    <nav class="navBar">
+      <span class="logo">logo</span>
+      <ul class="menu">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Services</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+  );
+}
+export default NavBar;
